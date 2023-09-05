f88640a39f6155acfeb33e1bfb9b8d5f48e38049
KojoBarbie <tmkhsy-0420@g.ecc.u-tokyo.ac.jp>
2023-09-05 17:13:32 +0900

    feat: taskコンポーネント

---
+++ teamc-app/src/components/TaskComponent.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+import { CardActionArea } from "@mui/material";
+
+type TaskComponentProps = {
+  id: number;
+  title: string;
+  userName: string;
+  skillSet: string[];
+  taskDate: Date;
+  concernDesc: string;
+};
+
+const TaskComponent = (taskComponentProps: TaskComponentProps) => {
+  return (
+    <Card sx={{ minWidth: 275, margin: "20px" }} variant="outlined">
+      <CardActionArea href={`/tasks/${taskComponentProps.id}`}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {taskComponentProps.title}
+          </Typography>
+          <Typography sx={{ ml: 1 }} color="text.secondary" gutterBottom>
+            {taskComponentProps.userName}
+          </Typography>
+          <Typography variant="body2">
+            {taskComponentProps.skillSet.map((skill) => (
+              <Chip label={skill} color="primary" />
+            ))}
+          </Typography>
+          <Typography sx={{ ml: 1 }} color="text.secondary" gutterBottom>
+            {taskComponentProps.taskDate.toLocaleDateString()}
+          </Typography>
+          <Typography sx={{ ml: 1 }}>
+            お悩み内容→{taskComponentProps.concernDesc}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default TaskComponent;
--- teamc-app/src/pages/TaskListPage.tsx
+++ teamc-app/src/pages/TaskListPage.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import TaskComponent from "../components/TaskComponent";
 
 const TaskListPage = () => {
-  return <div>TaskListPage</div>;
+  return (
+    <div>
+      <h1>TaskListPage</h1>
+      <TaskComponent
+        id={1}
+        title={"Reactわからん"}
+        userName={"細谷"}
+        skillSet={["React", "Docker"]}
+        taskDate={new Date()}
+        concernDesc={"piyopiyo"}
+      />
+    </div>
+  );
 };
 
 export default TaskListPage;
