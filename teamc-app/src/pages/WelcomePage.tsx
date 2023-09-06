d78589c77324d5f98f1adda151a9ee1f83e61f57
KojoBarbie <tmkhsy-0420@g.ecc.u-tokyo.ac.jp>
2023-09-06 11:59:36 +0900

    feat: welcome pageの作成

---
+++ teamc-app/src/pages/WelcomePage.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button, Typography, Box, Grid, Container } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const WelcomePage = () => {
+  return (
+    <Container maxWidth="sm">
+      <Box mt={5} textAlign="center">
+        <Typography variant="h4" mt={3}>
+          ようこそ、タスクヘルプツールへ！
+        </Typography>
+        <Grid container spacing={2} justifyContent="center" mt={3}>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/signup"
+            >
+              サインアップ
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to="/login"
+            >
+              ログイン
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+export default WelcomePage;
