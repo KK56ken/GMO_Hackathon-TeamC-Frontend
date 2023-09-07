fa95a128a7fae681742145641c0f2556b11d067f
KojoBarbie <tmkhsy-0420@g.ecc.u-tokyo.ac.jp>
2023-09-07 22:20:13 +0900

    feat: API連携(/tasks, /tasks/:id)

--- teamc-app/src/pages/TaskDetailPage.tsx
+++ teamc-app/src/pages/TaskDetailPage.tsx
@@ -2,76 +2,74 @@ import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import SkillSets from "../components/SkillSets";
-
-type TaskDetailPageProps = {
-  title: string;
-  userName: string;
-  taskDate: Date;
-  skillSet: string[];
-  concernDesc: string;
-  taskDetail: string;
-  ticketLink: string;
-  slackId: string;
-};
+import { useParams } from "react-router-dom";
+import { GetTaskById } from "../service/api/Task";
 
 const TaskDetailPage = () => {
-  const taskDetailPageProps: TaskDetailPageProps = {
-    title: "Task 1",
-    userName: "User 1",
-    taskDate: new Date(),
-    skillSet: ["skill 1", "skill 2"],
-    concernDesc: "Concern 1",
-    taskDetail:
-      "こんにちは。私はタスクの詳細です。\nよろしくお願いします。\n\n```ここにコードを書く```\n\n以上です。",
-    ticketLink: "https://www.google.com/",
-    slackId: "U05QQ2C3B61",
-  };
+  const urlParams = useParams<{ id: string }>();
+  if (urlParams.id === undefined) {
+    return <div>URLが不正です</div>;
+  } else {
+    const { data, isLoading, isError } = GetTaskById(Number(urlParams.id));
 
-  return (
-    <Container>
-      {/* TODO: ちゃんと下揃えをしたい */}
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <Typography variant="h2">{taskDetailPageProps.title}</Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          href={`slack://user?team=T05JTJTBSTS&id=${taskDetailPageProps.slackId}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          このタスクを助ける(DMへ)
-        </Button>
-        <Button variant="text" color="primary">
-          このタスクは解決した
-        </Button>
-      </div>
-      <Typography color="text.secondary" gutterBottom>
-        {taskDetailPageProps.userName},{" "}
-        {taskDetailPageProps.taskDate.toLocaleDateString()}
-      </Typography>
-      <SkillSets skillSet={taskDetailPageProps.skillSet} />
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <Typography variant="h5" marginTop="10px">
-          タスクの詳細
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    if (isError) {
+      return <div>Error loading tasks</div>;
+    }
+
+    if (!data) {
+      return <div>No tasks available</div>;
+    }
+
+    console.log(data);
+    return (
+      <Container>
+        {/* TODO: ちゃんと下揃えをしたい */}
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Typography variant="h2">{data.title}</Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            href={`slack://user?team=T05JTJTBSTS&id=${data.slack_id}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            このタスクを助ける(DMへ)
+          </Button>
+          <Button variant="text" color="primary">
+            このタスクは解決した
+          </Button>
+        </div>
+        <Typography color="text.secondary" gutterBottom>
+          {data.user_name}, {new Date(data.task_date).toLocaleDateString()}
         </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          href={taskDetailPageProps.ticketLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          チケットのページへ
-        </Button>
-      </div>
-      {/* ここはmarkdownに対応しても面白いかも */}
-      <Typography variant="body2">{taskDetailPageProps.taskDetail}</Typography>
-      <Typography variant="h5" marginTop="10px">
-        気になるところ
-      </Typography>
-      <Typography variant="body2">{taskDetailPageProps.concernDesc}</Typography>
-    </Container>
-  );
+        <SkillSets skillSet={data.skill_set} />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Typography variant="h5" marginTop="10px">
+            タスクの詳細
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            href={data.ticket_link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            チケットのページへ
+          </Button>
+        </div>
+        {/* ここはmarkdownに対応しても面白いかも */}
+        <Typography variant="body2">{data.task_detail}</Typography>
+        <Typography variant="h5" marginTop="10px">
+          気になるところ
+        </Typography>
+        <Typography variant="body2">{data.concern_desc}</Typography>
+      </Container>
+    );
+  }
 };
 
 export default TaskDetailPage;
