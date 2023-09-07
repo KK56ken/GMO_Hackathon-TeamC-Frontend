ed3ae22f008f30c547b1bd06386bc3114f8be3fe
k_h <K_Haruka03@outlook.jp>
2023-09-07 22:31:54 +0900

    タスク詳細表示のデザイン修正

--- teamc-app/src/pages/TaskDetailPage.tsx
+++ teamc-app/src/pages/TaskDetailPage.tsx
@@ -1,7 +1,9 @@
 import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import SkillSets from "../components/SkillSets";
+import Box from '@mui/material/Box';
 
 type TaskDetailPageProps = {
   title: string;
@@ -28,10 +30,10 @@ const TaskDetailPage = () => {
   };
 
   return (
-    <Container>
+    <Container sx={{ marginTop: 5}}>
       {/* TODO: ちゃんと下揃えをしたい */}
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <Typography variant="h2">{taskDetailPageProps.title}</Typography>
+      <div style={{ display: "flex", alignItems: "flex-end", marginBottom: 2 }}>
+        <Typography variant="h2" sx={{ marginRight: 3 }} style={{ lineHeight: 0.75 }}>{taskDetailPageProps.title}</Typography>
         <Button
           variant="contained"
           color="primary"
@@ -45,31 +47,40 @@ const TaskDetailPage = () => {
           このタスクは解決した
         </Button>
       </div>
-      <Typography color="text.secondary" gutterBottom>
+      <Typography color="text.secondary" gutterBottom sx={{ marginTop: 1}}>
         {taskDetailPageProps.userName},{" "}
         {taskDetailPageProps.taskDate.toLocaleDateString()}
       </Typography>
+      <hr/>
       <SkillSets skillSet={taskDetailPageProps.skillSet} />
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <Typography variant="h5" marginTop="10px">
-          タスクの詳細
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          href={taskDetailPageProps.ticketLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          チケットのページへ
-        </Button>
-      </div>
-      {/* ここはmarkdownに対応しても面白いかも */}
-      <Typography variant="body2">{taskDetailPageProps.taskDetail}</Typography>
-      <Typography variant="h5" marginTop="10px">
-        気になるところ
-      </Typography>
-      <Typography variant="body2">{taskDetailPageProps.concernDesc}</Typography>
+      <Card elevation={ 0 } sx={{ border: 1, borderColor: "#DDDDDD", borderRadius: 1, marginTop: 1, padding: 2 }}>
+        <div style={{ display: "flex", alignItems: "flex-end", marginBottom: 2 }}>
+          <Box sx={{ borderLeft: 5, paddingTop: 1, paddingBottom: 1, borderColor: "#BBBBBB" }}>
+            <Typography variant="h5" sx={{ marginRight: 3, marginLeft:1 }} style={{ lineHeight: 0.5 }}>
+              タスクの詳細
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            color="primary"
+            href={taskDetailPageProps.ticketLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            チケットのページへ
+          </Button>
+        </div>
+        {/* ここはmarkdownに対応しても面白いかも */}
+        <Typography variant="body2" sx={{marginTop: 2, marginBottom: 2}}>{taskDetailPageProps.taskDetail}</Typography>
+        <hr/>
+        <Box sx={{ height: 20}} />
+        <Box sx={{ borderLeft: 5, paddingTop: 1, paddingBottom: 1, borderColor: "#BBBBBB" }}>
+          <Typography variant="h5" sx={{ marginRight: 3, marginLeft:1 }} style={{ lineHeight: 0.5 }}>
+            気になるところ
+          </Typography>
+        </Box>
+        <Typography variant="body2" sx={{marginTop: 2}}>{taskDetailPageProps.concernDesc}</Typography>
+      </Card>
     </Container>
   );
 };
